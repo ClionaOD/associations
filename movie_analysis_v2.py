6baf3e81c212ceb9e9ce8f04042ebd3a219b3ef5
ClionaOD <CUSACKLAB\clionaodoherty@ip-10-0-1-119.simplead.cusacklab.org>
2019-07-23 15:57:10 +0000

    results for threshold 1%

--- movie_analysis_v2.py
+++ movie_analysis_v2.py
@@ -14,32 +14,30 @@ def create_baskets(dict1):
     return basket
 
 def fill_baskets(dict1, dict2):
-    #dict1 is the empty baskets for each movie (from create_baskets)
-    #dict2 is the same as create_baskets arg, each movie's labels.
+    #dict1 is the empty baskets for each movie (i.e. basket from create_baskets)
+    #dict2 is the same as create_baskets' arg, each movie's labels.
     for vals in dict2['alllabels']:
         dict1[vals['Timestamp']].extend([vals['Label']['Name']])
     
     return dict1
 
-def pool_baskets(dict):
-    inlist = list(dict.values())
-    rest = ['Logo', 'Text', 'Trademark', 'Word']
-    inlist = [x for x in inlist if x != rest]
-
+def pool_baskets(inlist, multiply_frames=1):
+    #inlist is the itemsets list of lists
+    #multiple_frames is what to multiply 200 ms by to get desired pooling e.g. multiply_frames=10 for 2 second baskets
     outlist = []
     startpoint = 0
 
-    for j in range(0,len(inlist)//10):
-        endpoint = startpoint + 10
+    for j in range(0,len(inlist)//multiply_frames):
+        endpoint = startpoint + multiply_frames
         outelement = []
         list_temp = inlist[startpoint:endpoint]
-        for i in range(0,10):
+        for i in range(0,multiply_frames):
             outelement.extend(list_temp[i])
         outlist.append(outelement)
-        startpoint = startpoint + 10
+        startpoint = startpoint + multiply_frames
 
-    if len(inlist)%10 != 0:
-        remainder = len(inlist)%10
+    if len(inlist)%multiply_frames != 0:
+        remainder = len(inlist)%multiply_frames
         remainderlist = inlist[-remainder:]
         outelement = []
         for i in range(0,remainder):
@@ -73,16 +71,29 @@ if __name__ == "__main__":
         baskets = fill_baskets(create_baskets(movie), movie)
         itemsets.extend(baskets.values())
 
-#    itemsets.extend(pool_baskets(amelie_baskets))
-
+    """
     #compute apriori and association rules
     te = TransactionEncoder()
     te_ary = te.fit(itemsets).transform(itemsets, sparse=True)
     df = pd.SparseDataFrame(te_ary, columns=te.columns_, default_fill_value=False)
 
-    frequent_itemsets = apriori(df, min_support=0.01, use_colnames=True, verbose=1, max_len=2)
+    frequent_itemsets = apriori(df, min_support=0.009, use_colnames=True, verbose=1, max_len=2)
     frequent_itemsets.to_csv(r'/home/CUSACKLAB/clionaodoherty/associations/results/apriori_itemsets.csv', sep=',', index=False)
 
     rules = association_rules(frequent_itemsets, metric='confidence', min_threshold=0.5)
     rules = rules[rules.confidence != 1]
     rules.to_csv(r'/home/CUSACKLAB/clionaodoherty/associations/results/association_rules.csv', sep=',', index=False)
+    """
+    for pool in range(1,11,3):
+        pooled_itemsets = pool_baskets(itemsets, pool)
+        
+        te = TransactionEncoder()
+        te_ary = te.fit(itemsets).transform(pooled_itemsets, sparse=True)
+        df = pd.SparseDataFrame(te_ary, columns=te.columns_, default_fill_value=False)
+
+        frequent_itemsets = apriori(df, min_support=0.1, use_colnames=True, verbose=1, max_len=2)
+        frequent_itemsets.to_csv(r'/home/CUSACKLAB/clionaodoherty/associations/results/apriori_itemsets_pooled_%.0f.csv' %pool, sep=',', index=False)
+
+        rules = association_rules(frequent_itemsets, metric='confidence', min_threshold=0.5)
+        rules = rules[rules.confidence != 1]
+        rules.to_csv(r'/home/CUSACKLAB/clionaodoherty/associations/results/association_rules_pooled_%.0f.csv' %pool, sep=',', index=False)
