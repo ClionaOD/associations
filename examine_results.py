4854189c7cb540cc4f0caff30baef0f92e05b8b4
ClionaOD <CUSACKLAB\clionaodoherty@ip-10-0-1-119.simplead.cusacklab.org>
2019-08-01 14:04:56 +0000

    find distribution of data

--- examine_results.py
+++ examine_results.py
@@ -2,28 +2,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-rules_one = pd.read_csv('./results/association_rules_1.csv', sep=',')
-rules = rules_one[['antecedents','consequents','leverage']]
-
-rules_one = pd.read_csv('./results/association_rules_1.csv', sep=',')
-rules_four = pd.read_csv('./results/association_rules_4.csv', sep=',')
-rules_seven = pd.read_csv('./results/association_rules_7.csv', sep=',')
-rules_ten = pd.read_csv('./results/association_rules_10.csv', sep=',')
+rules_one = pd.read_csv('./results/association_rules_one.csv', sep=',')
+rules_four = pd.read_csv('./results/association_rules_four.csv', sep=',')
+rules_seven = pd.read_csv('./results/association_rules_seven.csv', sep=',')
+rules_ten = pd.read_csv('./results/association_rules_ten.csv', sep=',')
 
 lev_one = rules_one[['antecedents','consequents','leverage']]
 lev_four = rules_four[['antecedents','consequents','leverage']]
 lev_seven = rules_seven[['antecedents','consequents','leverage']]
 lev_ten = rules_ten[['antecedents','consequents','leverage']]
 
-
 fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10,10))
 ax[0,0].hist(rules_one['leverage'], bins=100)
 ax[0,1].hist(rules_four['leverage'], bins=100)
 ax[1,0].hist(rules_seven['leverage'], bins=100)
 ax[1,1].hist(rules_ten['leverage'], bins=100)
-ax[0,0].set_title('200 ms')
-ax[0,1].set_title('800 ms')
-ax[1,0].set_title('1400 ms')
-ax[1,1].set_title('2000 ms')
+ax[0,0].set_title('200 ms, .01 support')
+ax[0,1].set_title('800 ms, .04 support')
+ax[1,0].set_title('1400 ms, .07 support')
+ax[1,1].set_title('2000 ms, .09 support')
 
-plt.savefig('./results/common_support_5_hists')
+plt.savefig('./results/individual_support_hists')
+plt.show()
