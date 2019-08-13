34a8ba6a28b44005e6514b9d5985ce0395458e9a
ClionaOD <CUSACKLAB\clionaodoherty@ip-10-0-1-119.simplead.cusacklab.org>
2019-08-13 16:09:11 +0000

    continue plot progress

--- plot_matrix.py
+++ plot_matrix.py
@@ -3,6 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
+#Create the dictionary for all possible pairwise comparisons
+itemsets = pd.read_csv('/home/CUSACKLAB/clionaodoherty/associations/results/frequent_itemsets/frequent_itemsets_one_indv.csv', sep=',')
+itemsets = itemsets[:181]
+items = itemsets['itemsets'].tolist()
+items = [i[12:-3] for i in items]
+
+series_1 = []
+for x in items:
+    for i in range(181):
+        series_1.append(x)
+
+series_2 = []
+for i in range(0, len(items)):
+    series_2.extend(items)
+
+all_combos_list = list(zip(series_1, series_2))
+all_combos_dict = {k: 0 for k in all_combos_list}
+
+#Create the dictionary for actual leverage values
 lev = pd.read_csv('./results/association_rules/association_rules_one_indv.csv', sep=',')
 leverage = lev[['antecedents','consequents','leverage']]
 
@@ -10,23 +29,30 @@ ant = leverage['antecedents'].tolist()
 ant = [i[12:-3] for i in ant]
 cons = leverage['consequents'].tolist()
 cons = [i[12:-3] for i in cons]
-pairs = list(zip(ant, cons))
+ant_con_list = list(zip(ant, cons))
 
 lev = leverage['leverage'].tolist()
-lev = [0 if i<0 else i for i in lev]
+lev_list = [0 if i<0 else i for i in lev]
 
-data = {k:v for k,v in zip(pairs,lev)}
+lev_dict = {k:v for k,v in zip(ant_con_list,lev)} #actual data points
 
-keys = np.array(list(data.keys()))
-vals = np.array(list(data.values()))
+#Compare and update leverage values
+symmetric_pairs_list = [(x,y) for x, y in all_combos_dict.keys() if x == y]
+symmetric_pairs_dict = {k:1 for k in symmetric_pairs_list}
 
-unq_keys, key_idx = np.unique(keys, return_inverse=True)
-key_idx = key_idx.reshape(-1, 2)
-n = len(unq_keys)
-adj = np.zeros((n, n))
-adj[key_idx[:,0], key_idx[: ,1]] = vals
-adj += adj.T
+pairs_not_zero = lev_dict + symmetric_pairs_dict
 
+not_in_rekog_data = {key:0 for key in series_dict if not key in lev_dict or not symmetric_pairs_dict} #
+
+
+        
+
+
+lev_df = pd.DataFrame()
+lev_df['Series One'] = series_1
+lev_df['Series Two'] = series_2 
+
+'''
 matrix = np.eye(183,183)
 fig = plt.figure()
 ax = fig.add_subplot(121)
