90c84bb825c4e02e895534088007b2179c13fdd3
ClionaOD <odoherc1@tcd.ie>
2019-12-09 17:32:44 +0000

    do ridge regression per time lag

--- var.py
+++ var.py
@@ -2,13 +2,12 @@ import random
 import numpy as np
 import pandas as pd
 import pickle
-from statsmodels.tsa.api import VAR
 from mlxtend.preprocessing import TransactionEncoder
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
 from scipy import signal
-from statsmodels.discrete.discrete_model import BinaryModel
+from sklearn.linear_model import Ridge
 
 def divide_dataset(lst, div):
     length = int(len(lst)/div)
@@ -46,7 +45,14 @@ def one_hot_enc(lst, items):
     itemMaps = {k:v for k,v in enumerate(items)}
     return onehot_arr
 
+def ridge_regress(X,y):
+    clf = Ridge(alpha=1,fit_intercept=False)
+    clf.fit(X,y)
+    coefs = clf.coef_
+    return coefs
+    
 
+"""
 def perform_var(arr, nlags, div):
     model = VAR(arr)
     results = model.fit(maxlags=nlags)
@@ -63,6 +69,7 @@ def perform_var(arr, nlags, div):
     plt.close()
 
     return results, out_coefs
+"""
 
 if __name__ == "__main__":
 
@@ -78,6 +85,34 @@ if __name__ == "__main__":
     nlags=4
     decimateby=5
 
+    allcoefs = np.zeros((nlags,nitems,nitems,len(div_itemsets)))
+    
+    for i in range(0, len(div_itemsets)):
+        arr = one_hot_enc(div_itemsets[i], frequent_items)
+        nobs = len(arr)
+        div_coefs = np.zeros((nlags, nitems, nitems))
+        
+        if decimateby:
+            arr = signal.decimate(arr,decimateby,axis=0)
+        
+        for lag in range(1,nlags+1):
+            y = arr[nlags:,:]
+            X = arr[nlags-lag:-lag,:]
+            coef = ridge_regress(X,y)
+            div_coefs[lag-1,:,:] = coef
+        
+        fig,ax = plt.subplots(ncols=nlags)
+        if nlags == 1:
+            ax = [ax]
+        for lag in range(nlags):
+            sns.heatmap(div_coefs[lag], ax=ax[lag])
+        plt.savefig('./results/ridge_regression/coefs/coefs_div{}.pdf'.format(i))
+        plt.close()
+        
+        allcoefs[:,:,:,i] = div_coefs
+
+    """
+
     allcoefs=np.zeros((nlags,nitems,nitems,len(div_itemsets)))
     for i in range(0,len(div_itemsets)):
         arr = one_hot_enc(div_itemsets[i], frequent_items)
@@ -89,6 +124,7 @@ if __name__ == "__main__":
             #plt.plot(arr[1:500,:])
         results, coefs = perform_var(arr, nlags=nlags, div=i)
         allcoefs[:,:,:,i]=coefs
+    """
 
     coef_tstats=stats.ttest_1samp(allcoefs, 0, axis=3)
 
@@ -97,15 +133,15 @@ if __name__ == "__main__":
         ax=[ax] 
     for lag in range(nlags):
         sns.heatmap(coef_tstats.pvalue[lag],ax=ax[lag], cmap='BuPu', vmin=0, vmax=0.1)
-    plt.savefig('./results/var/mean_tstats_pvals_{}lags.pdf'.format(nlags))
+    plt.savefig('./results/ridge_regression/mean_tstats_pvals_{}lags.pdf'.format(nlags))
 
     mn_allcoefs=np.mean(allcoefs,axis=3)
     fig,ax=plt.subplots(ncols=nlags, figsize=[12,8])
     if nlags==1:
         ax=[ax] 
     for lag in range(nlags):
-        sns.heatmap(mn_allcoefs[lag],ax=ax[lag], vmin=-4, vmax=4, cmap='seismic')
-    plt.savefig('./results/var/mean_coefs_{}lags.pdf'.format(nlags))
+        sns.heatmap(mn_allcoefs[lag],ax=ax[lag], vmin=-1, vmax=1, cmap='seismic')
+    plt.savefig('./results/ridge_regression/mean_coefs_{}lags.pdf'.format(nlags))
 
     plt.show()
 
