1630e4d3ebb437c935044fdb0291870b2e59d253
ClionaOD <odoherc1@tcd.ie>
2020-03-11 13:25:26 +0000

    get top items in time course

--- check_items.py
+++ check_items.py
@@ -1,12 +1,37 @@
 import pickle
+import os
+import numpy as np
+import pandas as pd
 
-with open('/home/CUSACKLAB/clionaodoherty/associations/diagonal_over_0.01.pickle','rb') as f:
-    items=pickle.load(f)
+items = []
+fileOrder = []
 
-with open('/home/CUSACKLAB/clionaodoherty/associations/freq_order.pickle','rb') as f:
-    order=pickle.load(f)dsa
+with open('./freq_order.pickle','rb') as f:
+    order=pickle.load(f)
 
+for file in os.listdir('./top_items'):
 
-for x in items:
+    with open('./top_items/{}'.format(file),'rb') as f:
+        idx = pickle.load(f)
+        fileOrder.append(os.path.basename(f.name))
 
-    print(order[x]) #, order[y])
+    if type(idx) != list:
+        idx = idx[0].tolist()
+
+    A = []
+    if not type(idx[0]) == int:
+        for x,y in idx:
+            if not order[x] == order[y]:
+                A.append((order[x],order[y]))
+    else:
+        for x in idx:
+            A.append(order[x])
+
+    items.append(A)
+
+data = {}
+for idx, i in enumerate(items):
+    S = pd.Series(i, name=fileOrder[idx])
+    data[S.name] = S
+
+df = pd.DataFrame(data)
